b2e3aba7fdf4b169115247982f2e9e5e6f8f5d21
mohan Gautam <rajesh.gautam010@gmail.com>
2021-10-06 12:18:24 +0200

    2nd commit added some UI and some databases function!!!!

--- app/src/main/java/com/example/sitandecider/MainActivity.java
+++ app/src/main/java/com/example/sitandecider/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.sitandecider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //-- Some variables to be decided here
+
+                 //var platz!!!!
+
+          //  -- till here
+
+
+
+    Button btn_rand, btn_showall;
+    TextView tv_decide;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        btn_rand = findViewById(R.id.btn_rand);
+        btn_showall = findViewById(R.id.btn_showall);
+
+        tv_decide = findViewById(R.id.tv_decide);
+
+
+        btn_showall.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SavedDinner.class);
+                startActivity(i);
+            }
+        });
+
+
+    }  //------------------------------------------- end of on Create-----------------------------------
 }
