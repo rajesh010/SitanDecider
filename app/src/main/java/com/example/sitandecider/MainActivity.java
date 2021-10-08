bcad75136c890f14c54ad5645e0a1dd248cf36cc
mohan Gautam <rajesh.gautam010@gmail.com>
2021-10-08 16:20:56 +0200

    3rd commit added some functions and attached together

--- app/src/main/java/com/example/sitandecider/MainActivity.java
+++ app/src/main/java/com/example/sitandecider/MainActivity.java
@@ -7,12 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     //-- Some variables to be decided here
 
                  //var platz!!!!
+    int maxOfDatabase;
+    String maxNameOfDatabase;
+    int randomNumber;
+   static boolean truenum;
+
+    String RandomName;
+
+    ArrayList<Integer> includedIDs = new ArrayList<Integer>();
 
           //  -- till here
 
@@ -20,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn_rand, btn_showall;
     TextView tv_decide;
+
+    DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
 
     @Override
@@ -33,15 +47,69 @@ public class MainActivity extends AppCompatActivity {
         tv_decide = findViewById(R.id.tv_decide);
 
 
+
+
+        btn_rand.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+                maxOfDatabase = dataBaseHelper.MaxNum();
+
+                int min = 0;
+                randomNumber = (int) Math.floor(Math.random()*(maxOfDatabase-min+1)+min);
+
+                rand();
+
+                if (truenum == false) {
+                    randomNumber = (int) Math.floor(Math.random() * (maxOfDatabase - min + 1) + min);
+                    rand();
+                } else {
+                    RandomName = dataBaseHelper.maxName(randomNumber);
+                    tv_decide.setText(RandomName);
+                }
+
+            }
+        });
+
+
         btn_showall.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, SavedDinner.class);
                 startActivity(i);
+                Toast.makeText(MainActivity.this, "Deleted: " + randomNumber + "and rand name " + RandomName , Toast.LENGTH_SHORT).show();
+
             }
         });
 
 
+
+
+
     }  //------------------------------------------- end of on Create-----------------------------------
+
+    public void rand(){
+
+        includedIDs = dataBaseHelper.onlyInclude();
+        Collections.sort(includedIDs);
+        int n = 0 ;
+        try{
+        while (includedIDs.get(n) <= maxOfDatabase) {
+            if (includedIDs.get(n) == randomNumber){
+                RandomName = dataBaseHelper.maxName(randomNumber);
+                truenum = true;
+                break;
+        }else {
+                n++;
+                truenum = false;
+            }
+        } }
+        catch(Exception e) {
+             n = 0;
+        }
+
+    }
+
 }
